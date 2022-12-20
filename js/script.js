ae5452158c470cbaca871280072463c8d98e6f26
MikeSaltykov07 <37000318+MikeSaltykov07@users.noreply.github.com>
2022-12-20 17:20:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const start = new Date();
+window.addEventListener('DOMContentLoaded', () => {
+    console.log(`Время полной загрузки DOM: ${Date.now() - start} ms`)
+})  
+window.addEventListener('load', () => {
+    console.log(`Время полной загрузки страницы: ${Date.now() - start} ms`)
+})
